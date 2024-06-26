66d170bd5adb4c210700dc911d8c4ccaf4d1355b
Mohmed932 <mohmedmahmoudfouad@gmail.com>
2024-06-26 21:30:28 +0300

    increase performance

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,23 +1,7 @@
-/** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: [
-    "./pages/**/*.{js,ts,jsx,tsx,mdx}",
-    "./components/**/*.{js,ts,jsx,tsx,mdx}",
-    "./app/**/*.{js,ts,jsx,tsx,mdx}",
-  ],
-  plugins: [
-    // require('@tailwindcss/line-clamp'),
-  ],
+  content: ["./src/**/*.{html,js}"],
   theme: {
-    extend: {
-      backgroundImage: {
-        "gradient-radial": "radial-gradient(var(--tw-gradient-stops))",
-        "gradient-conic":
-          "conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))",
-      },
-      direction: {
-        ltr: 'ltr',
-      },
-    },
+    extend: {},
   },
-};
+  plugins: [],
+}
